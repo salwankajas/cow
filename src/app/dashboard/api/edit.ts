42259411f5c88bc25cffca1edab261118b74b243
salwankajas <salwankajas@gmail.com>
2023-08-24 15:43:39 +0530

    type error fixed

--- src/app/dashboard/api/edit.ts
+++ src/app/dashboard/api/edit.ts
@@ -1,16 +1,31 @@
 import { getDatabase, ref, set } from "firebase/database";
 import removes from "./remove";
+// interface EventForm{
+//   id:string;
+//   heading:string;
+//   day:number;
+//   month:string;
+//   time?:string;
+//   poster?:string;
+//   content:string;
+//   vanue?:string;
+//   link?:string;
+//   type:string;
+//   year?:number;
+//   soceity:string;
+// }
+
 interface EventForm{
   id:string;
-  heading:string;
-  day:number;
-  month:string;
+  heading?:string;
+  day?:number;
+  month?:string;
   time?:string;
   poster?:string;
-  content:string;
+  content?:string;
   vanue?:string;
   link?:string;
-  type:string;
+  type?:string;
   year?:number;
   soceity:string;
 }
@@ -18,7 +33,7 @@ function getTimestamp(year:number,month:number,day:number){
   let date = Date.parse(`${month}/${day}/${year} ${Math.floor(Math.random() * (13))}:${Math.floor(Math.random() * (61))}:${Math.floor(Math.random() * (61))}`)
   return date/1000
 }
-const months = {"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sept":9,"Oct":10,"Nov":11,"Dec":12}
+const months: { [key: string]: number } = {"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sept":9,"Oct":10,"Nov":11,"Dec":12}
 
 function editUserData({id,heading,day,month,time,poster,content,link,vanue,type,year,soceity}:EventForm) {
   if(soceity == "sb" || soceity=="cs"){
@@ -36,7 +51,7 @@ function editUserData({id,heading,day,month,time,poster,content,link,vanue,type,
         });
       }else if(type=="archives"){
         removes(id,type,soceity)
-        set(ref(db, `${soceity}/archives/` + getTimestamp(year!,months[month],day!)), {
+        set(ref(db, `${soceity}/archives/` + getTimestamp(year!,months[month!],day!)), {
           heading:heading,
           day:day,
           month:month,
--- src/app/dashboard/api/write.ts
+++ src/app/dashboard/api/write.ts
@@ -21,7 +21,7 @@ function getTimestamp(year:number,month:number,day:number){
   let date = Date.parse(`${month}/${day}/${year} ${Math.floor(Math.random() * (13))}:${Math.floor(Math.random() * (61))}:${Math.floor(Math.random() * (61))}`)
   return date/1000
 }
-const months = {"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sept":9,"Oct":10,"Nov":11,"Dec":12}
+const months: { [key: string]: number }  = {"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sept":9,"Oct":10,"Nov":11,"Dec":12}
 function writeUserData({heading,day,month,time,poster,content,link,vanue,type,year,soceity}:EventForm) {
   if(soceity == "sb" || soceity=="cs"){
     const db = getDatabase();
@@ -38,7 +38,7 @@ function writeUserData({heading,day,month,time,poster,content,link,vanue,type,ye
       });
     }else if(type=="archives"){
       // getTimestamp(year!,months[month!],day!)
-      set(ref(db, `${soceity}/archives/` + getTimestamp(year!,months[month],day!)), {
+      set(ref(db, `${soceity}/archives/` + getTimestamp(year!,months[month!],day!)), {
         heading:heading,
         day:day,
         month:month,
